e71fd5dbb75ad4fda3a35931e0bb0cfc7623437c
Matias Mas <matias.mas.luberiaga@gmail.com>
2024-02-16 01:33:18 -0300

    Resolviendo Requerimiento 3, insercion de nuevo alumno.

--- src/sistema/logica/CapaLogica.java
+++ src/sistema/logica/CapaLogica.java
@@ -1,5 +1,42 @@
 package sistema.logica;
 
+import sistema.excepciones.AlumnoExistenteException;
+import sistema.excepciones.NoHayAlumnosException;
+import sistema.logica.alumno.Alumno;
+import sistema.logica.alumno.Alumnos;
+import sistema.logica.alumno.Becado;
+import sistema.logica.asignatura.Asignaturas;
+import sistema.valueobjects.VOAlumnoRegistro;
+
 public class CapaLogica {
+	
+	private Alumnos diccioAlumnos;
+	private Asignaturas diccioAsignaturas;
+	
+	public CapaLogica() {
+		
+	}
+	
+	//R3 = Registrar alumno en la universidad.
+	public void registrarAlumno(VOAlumnoRegistro alumno) {
+		if(diccioAlumnos.empty()) {
+			throw new NoHayAlumnosException("No hay alumnos en el sistema.");
+		} else {
+			if(diccioAlumnos.member(alumno.getCedula())) {
+				throw new AlumnoExistenteException("Un alumno con esa cedula ya se encuentra registrado.");
+			} else {
+				switch(alumno.getTipoAlumno()) {
+					case BECADO:
+						Becado nuevoBecado = new Becado(alumno.getCedula(), alumno.getNombre(), alumno.getApellido(), alumno.getDomicilio(), alumno.getTelefono(), alumno.getPorcentajeBeca(), alumno.getRazonBeca());
+						diccioAlumnos.insert(nuevoBecado);
+						break;
+					case NORMAL:
+						Alumno nuevoAlumno = new Alumno(alumno.getCedula(), alumno.getNombre(), alumno.getApellido(), alumno.getDomicilio(), alumno.getTelefono());
+						diccioAlumnos.insert(nuevoAlumno);
+						break;
+				}
+			}
+		}
+	}
 
 }
