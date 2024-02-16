90dff817db16c0eb34a5055ea88a8179d67e2c56
daroher <107977888+daroher@users.noreply.github.com>
2024-02-16 12:55:42 -0300

    inscripcion e inscripciones sin implementacion
    Comiteo inscripcion e inscripciones sin la implementacion de los metodos especificos para resolver requerimientos aun.

--- src/sistema/logica/inscripcion/Inscripcion.java
+++ src/sistema/logica/inscripcion/Inscripcion.java
@@ -1,5 +1,71 @@
 package sistema.logica.inscripcion;
 
+import sistema.logica.asignatura.Asignatura;
+
 public class Inscripcion {
+
+	int numeroInscripcion;
+	int anioLectivo;
+	int montoBase;
+	int calificacion;
+	Asignatura asignatura;
+
+	public Inscripcion() {
+		super();
+	}
+
+	public Inscripcion(int numeroInscripcion, int anioLectivo, int montoBase, int calificacion, Asignatura asignatura) {
+		super();
+		this.numeroInscripcion = numeroInscripcion;
+		this.anioLectivo = anioLectivo;
+		this.montoBase = montoBase;
+		this.calificacion = calificacion;
+		this.asignatura = asignatura;
+	}
+
+	public void registrarResultado(int nota) {
+		// TODO: falta implementar
+	}
+	
+	public int getNumeroInscripcion() {
+		return numeroInscripcion;
+	}
+
+	public void setNumeroInscripcion(int numeroInscripcion) {
+		this.numeroInscripcion = numeroInscripcion;
+	}
+
+	public int getAnioLectivo() {
+		return anioLectivo;
+	}
+
+	public void setAnioLectivo(int anioLectivo) {
+		this.anioLectivo = anioLectivo;
+	}
+
+	public int getMontoBase() {
+		return montoBase;
+	}
+
+	public void setMontoBase(int montoBase) {
+		this.montoBase = montoBase;
+	}
+
+	public int getCalificacion() {
+		return calificacion;
+	}
+
+	public void setCalificacion(int calificacion) {
+		this.calificacion = calificacion;
+	}
+
+	public Asignatura getAsignatura() {
+		return asignatura;
+	}
+
+	public void setAsignatura(Asignatura asignatura) {
+		this.asignatura = asignatura;
+	}	
+	
 
 }
--- src/sistema/logica/inscripcion/Inscripciones.java
+++ src/sistema/logica/inscripcion/Inscripciones.java
@@ -1,5 +1,31 @@
 package sistema.logica.inscripcion;
 
+import java.util.LinkedList;
+
 public class Inscripciones {
+
+	LinkedList<Inscripcion> inscripciones;
+
+	public Inscripciones() {
+		super();
+	}
+
+	public Inscripciones(LinkedList<Inscripcion> inscripciones) {
+		super();
+		this.inscripciones = inscripciones;
+	}
+
+	public float calcularMontoRecaudado(int ced, int anioLectivo) {
+		// TODO: falta implementar
+		return 0;
+	}
+
+	public LinkedList<Inscripcion> getInscripciones() {
+		return inscripciones;
+	}
+
+	public void setInscripciones(LinkedList<Inscripcion> inscripciones) {
+		this.inscripciones = inscripciones;
+	}
 
 }
