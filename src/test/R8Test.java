e7a921e61440ea22574354dcd030d77a1cabe379
daroher <107977888+daroher@users.noreply.github.com>
2024-02-22 00:15:14 -0300

    arreglo en R8, aun no esta terminado

--- src/test/R8Test.java
+++ src/test/R8Test.java
@@ -2,6 +2,7 @@ package test;
 
 import sistema.logica.CapaLogica;
 import sistema.valueobjects.VOCalcularMontoRecaudado;
+import sistema.valueobjects.VOInscribirAlumno;
 import sistema.valueobjects.VOMontoRecaudado;
 
 public class R8Test {
@@ -10,6 +11,17 @@ public class R8Test {
 
 		VOCalcularMontoRecaudado voCalcularMontoRecaudado;
 		VOMontoRecaudado voMontoRecaudado;
+		VOInscribirAlumno voInscribirAlumno;
+		
+		//inscribo un becado a algunas asignaturas
+		try {
+			voInscribirAlumno = new VOInscribirAlumno("asig1", 12223335, 2024, 10000);
+			capaLogica.inscribirAlumnoEnAsignatura(voInscribirAlumno);
+			voInscribirAlumno = new VOInscribirAlumno("asig2", 12223335, 2024, 20000);
+			capaLogica.inscribirAlumnoEnAsignatura(voInscribirAlumno);
+		} catch (Exception e) {
+			System.out.println("Error inscribiendo alumno becado a asignaturas de prueba");
+		}
 		
 		try {
 			// calculo monto de normal
