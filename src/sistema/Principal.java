5bf2ea945563e987764365285be5550b81721d55
daroher <107977888+daroher@users.noreply.github.com>
2024-03-01 21:44:35 -0300

    Agrego estilos a las ventanas

--- src/sistema/Principal.java
+++ src/sistema/Principal.java
@@ -1,10 +1,21 @@
 package sistema;
 
+import javax.swing.UIManager;
+
+import sistema.grafica.pantallas.VentanaPrincipal;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		sistema.grafica.pantallas.VentanaPrincipal v = new sistema.grafica.pantallas.VentanaPrincipal();
+		try {
+			//seteo estilos para las panatallas
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Exception e) {
+			//TODO:hacer excepcion cargando los estilos
+		}
+
+		VentanaPrincipal v = new VentanaPrincipal();
 		v.setVisible(true);
 	}
-	
+
 }
